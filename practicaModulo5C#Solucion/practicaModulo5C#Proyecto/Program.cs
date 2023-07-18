b827e20e0cec1f8c322a59cc395aa075d6b80cd0
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-07-18 10:31:59 +0200

    Creado proyecto

---
+++ practicaModulo5C#Solucion/practicaModulo5C#Proyecto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace practicaModulo5C_Proyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
