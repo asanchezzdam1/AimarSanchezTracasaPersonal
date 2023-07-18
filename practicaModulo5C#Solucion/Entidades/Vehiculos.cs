66494c427cb4ce53dfa2bd38c662e27b1d4cc8ef
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-07-18 12:19:14 +0200

    Terminado proyecto "practicaModulo5C#"

---
+++ practicaModulo5C#Solucion/Entidades/Vehiculos.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Entidades
+{
+    public class Vehiculos
+    {
+        public enum Etiqueta
+        {
+            cero,
+            eco,
+            c,
+            b,
+            sinEtiqueta
+        }
+        public Vehiculos()
+        {
+            
+        }
+        public Vehiculos(string propietario,string marca, DateTime fechaMatriculacion, Etiqueta etiqueta)
+        {
+            this.propietario = propietario;
+            this.marca = marca;
+            this.fechaMatriculacion = fechaMatriculacion;
+            this.etiqueta = etiqueta;
+        }
+        public string propietario { get; set; }
+        public string marca { get; set; }
+        public DateTime fechaMatriculacion { get; set; }
+        public Etiqueta etiqueta { get; set; }
+
+        public string calcularInpuestos(Vehiculos vehiculo)
+        {
+            DateTime fechaActual = DateTime.Now;
+            double precioBase = 1000;
+            double anosAntiguedad = (fechaActual.Year - vehiculo.fechaMatriculacion.Year);
+            if(anosAntiguedad < 0) anosAntiguedad = 0;
+            double totalDePorcentajes = 0;
+            switch (vehiculo.etiqueta)
+            {
+                case Etiqueta.cero:
+                    totalDePorcentajes = 0;
+                    break;
+                case Etiqueta.eco:
+                    totalDePorcentajes = 5;
+                    break;
+                case Etiqueta.c:
+                    totalDePorcentajes = 10;
+                    break;
+                case Etiqueta.b:
+                    totalDePorcentajes = 15;
+                    break;
+                case Etiqueta.sinEtiqueta:
+                    totalDePorcentajes = 25;
+                    break;
+            }
+            string mes = "";
+            switch (vehiculo.fechaMatriculacion.Month)
+            {
+                case 1:
+                    mes = "Enero";
+                    break;
+                case 2:
+                    mes = "Febrero";
+                    break;
+                case 3:
+                    mes = "Marzo";
+                    break;
+                case 4:
+                    mes = "Abril";
+                    break;
+                case 5:
+                    mes = "Mayo";
+                    break;
+                case 6:
+                    mes = "Junio";
+                    break;
+                case 7:
+                    mes = "Julio";
+                    break;
+                case 8:
+                    mes = "Agosto";
+                    break;
+                case 9:
+                    mes = "Septiembre";
+                    break;
+                case 10:
+                    mes = "Octubre";
+                    break;
+                case 11:
+                    mes = "Noviembre";
+                    break;
+                case 12:
+                    mes = "Diciembre";
+                    break;
+            }
+            totalDePorcentajes += anosAntiguedad;
+            double totalDeInpuestos = precioBase + (precioBase * (totalDePorcentajes / 100));
+            return "El vehiculo de la marca " 
+                    + vehiculo.marca 
+                    + ", matriculado el " 
+                    + vehiculo.fechaMatriculacion.Day + " de " 
+                    + mes + " del " 
+                    + vehiculo.fechaMatriculacion.Year 
+                    + " tiene que pagar " 
+                    + totalDeInpuestos + " euros en concepto de inpuestos de circulación.";
+        }
+    }
+}
--- practicaModulo5C#Solucion/practicaModulo5C#Proyecto/Program.cs
+++ practicaModulo5C#Solucion/practicaModulo5C#Proyecto/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace practicaModulo5C_Proyecto
+﻿using Entidades;
+namespace practicaModulo5C_Proyecto
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Vehiculos generico = new Vehiculos();
+            Vehiculos vehiculo1 = new Vehiculos("Juan","Kia",new DateTime(2017,09,29), Vehiculos.Etiqueta.c);
+            Vehiculos vehiculo2 = new Vehiculos("Nerea","Opel",new DateTime(2008,12,26), Vehiculos.Etiqueta.b);
+            Vehiculos vehiculo3 = new Vehiculos("Ibai","Ford",new DateTime(1993,09,29), Vehiculos.Etiqueta.sinEtiqueta);
+            Vehiculos vehiculo4 = new Vehiculos("Pedro","Seat",new DateTime(2014,01,12), Vehiculos.Etiqueta.b);
+            Vehiculos vehiculo5 = new Vehiculos("Amaia","Toyota",new DateTime(2020,01,12), Vehiculos.Etiqueta.eco);
+            Vehiculos vehiculo6 = new Vehiculos("Daniel","Tesla",new DateTime(2023,05,25), Vehiculos.Etiqueta.cero);
+
+            var lista = new List<Vehiculos>();
+            lista.Add(vehiculo1);
+            lista.Add(vehiculo2);
+            lista.Add(vehiculo3);
+            lista.Add(vehiculo4);
+            lista.Add(vehiculo5);
+            lista.Add(vehiculo6);
+            
+            foreach (var vehiculo in lista)
+            {
+                Console.WriteLine(generico.calcularInpuestos(vehiculo));  
+            }
         }
     }
 }
