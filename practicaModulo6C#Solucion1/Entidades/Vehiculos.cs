64919066012b46059469d4f961c0165f59160476
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-07-19 09:36:18 +0200

    Entidad de vehiculos completada

--- practicaModulo6C#Solucion1/Entidades/Vehiculos.cs
+++ practicaModulo6C#Solucion1/Entidades/Vehiculos.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Entidades
 {
-    internal class Vehiculos
+    public class Vehiculos
     {
+        public int anoCompra { get; set; }
+        public string color { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+
+        public Vehiculos(int anoCompra, string color)
+        {
+            this.anoCompra = anoCompra;
+            this.color = color;
+        }
+        public Vehiculos(string marca, string modelo)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+        public Vehiculos(int anoCompra, string color, string marca, string modelo)
+        {
+            this.anoCompra = anoCompra;
+            this.color = color;
+            this.marca = marca;
+            this.modelo = modelo;
+        }
     }
 }
