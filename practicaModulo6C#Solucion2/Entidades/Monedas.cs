f1f153cbf8e42b11eb8d0ad17bc40695edd1be1e
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-07-19 13:30:50 +0200

    Terminado proyecto "practicaModulo6C#2"

---
+++ practicaModulo6C#Solucion2/Entidades/Monedas.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Entidades
+{
+    public class Monedas
+    {
+        const double eurosDolar = 1.12;
+        const double eurosLibra = 0.87;
+        const double dolarEuros = 0.89;
+        const double dolarLibra = 0.77;
+        const double libraEuros = 1.15;
+        const double libraDolar = 1.29;
+        public enum Moneda
+        {
+            Euro,
+            Dolar,
+            Libra
+        }
+        public int monedaOrigen { get; set; }
+        public int monedaDestino { get; set; }
+        public int cantidad { get; set; }
+        public Moneda origen { get; set; }
+        public Moneda destino { get; set; }
+
+        public Monedas(int monedaOrigen, int monedaDestino, int cantidad)
+        {
+            this.monedaOrigen = monedaOrigen;
+            this.monedaDestino = monedaDestino;
+            this.cantidad = cantidad;
+        }
+        public string calcular()
+        {
+            double resultado = 0;
+            switch (monedaOrigen)
+            {
+                case 1:
+                    origen = Moneda.Euro;
+                    break;
+                case 2:
+                    origen = Moneda.Dolar;
+                    break;
+                case 3:
+                    origen = Moneda.Libra;
+                    break;
+            }
+            switch (monedaDestino)
+            {
+                case 1:
+                    destino = Moneda.Euro;
+                    break;
+                case 2:
+                    destino = Moneda.Dolar;
+                    break;
+                case 3:
+                    destino = Moneda.Libra;
+                    break;
+            }
+            if (origen != destino)
+            {
+                if (origen == Moneda.Euro)
+                {
+                    if (destino == Moneda.Dolar) resultado = cantidad * eurosDolar;
+                    else resultado = cantidad * eurosLibra;
+                }
+                else if (origen == Moneda.Dolar)
+                {
+                    if (destino == Moneda.Euro) resultado = cantidad * dolarEuros;
+                    else resultado = cantidad * dolarLibra;
+                }
+                else
+                {
+                    if (destino == Moneda.Euro) resultado = cantidad * libraEuros;
+                    else resultado = cantidad * libraDolar;
+                }
+            }
+            else return "Error en la conversion";
+            return "La moneda de origen es: " + origen + " y de destino " + destino + ".\nEl total de la conversión son " + resultado + monedaDestino;
+        }
+    }
+}
---
+++ practicaModulo6C#Solucion2/practicaModulo6C#Proyecto2/Program.cs
@@ -0,0 +1,119 @@
+﻿using Entidades;
+
+namespace practicaModulo6C_Proyecto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            bool primeraEjecucion = true;
+            while (true)
+            {
+                if (!primeraEjecucion)
+                {
+                    Console.WriteLine("Pulse enter");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+                string opText = "";
+                string opTextOrigen = "";
+                string opTextDestino = "";
+                string cantidadText = "";
+
+                int op = -1;
+                int opOrigen = -1;
+                int opDestino = -1;
+                int cantidad = 0;
+                do
+                {
+                    program.menu1();
+                    opText = Console.ReadLine();
+                    try
+                    {
+                        op = Int32.Parse(opText);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Formato no valido");
+                    }
+                }
+                while (op < 1 || op > 2);
+                switch (op)
+                {
+                    case 1:
+                        do
+                        {
+                            program.menu2("origen");
+                            opTextOrigen = Console.ReadLine();
+                            try
+                            {
+                                opOrigen = Int32.Parse(opTextOrigen);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Formato no valido");
+                            }
+                        }
+                        while (opOrigen < 1 || opOrigen > 3);
+
+                        do
+                        {
+                            program.menu2("destino");
+                            opTextDestino = Console.ReadLine();
+                            try
+                            {
+                                opDestino = Int32.Parse(opTextDestino);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Formato no valido");
+                            }
+                        }
+                        while (opDestino < 1 || opDestino > 3);
+
+                        while (true)
+                        {
+                            Console.WriteLine("Seleccione la cantidad a covertir (SOLO NUMEROS ENTEROS)");
+                            cantidadText = Console.ReadLine();
+                            try
+                            {
+                                cantidad = Int32.Parse(cantidadText);
+                                break;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Formato no valido");
+                            }
+                        }
+                        var moneda = new Monedas(opOrigen,opDestino, cantidad);
+                        Console.WriteLine(moneda.calcular());
+                        primeraEjecucion = false;
+                        break;
+                    case 2:
+                        Console.WriteLine("Gracias por usar nuestra aplicación");
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+        private void menu1()
+        {
+            Console.WriteLine("************* MENU *************");
+            Console.WriteLine("* 1- Convertir                 *");
+            Console.WriteLine("* 2- Salir                     *");
+            Console.WriteLine("********************************");
+            Console.WriteLine("Seleccione una opción: ");
+        }
+        private void menu2(string accion)
+        {
+            Console.WriteLine("************* MENU *************");
+            Console.WriteLine("* 1- Euro                      *");
+            Console.WriteLine("* 2- Dolar                     *");
+            Console.WriteLine("* 3- Libra                     *");
+            Console.WriteLine("********************************");
+            Console.WriteLine("Seleccione la moneda de " + accion + ": ");
+        }
+    }
+}
