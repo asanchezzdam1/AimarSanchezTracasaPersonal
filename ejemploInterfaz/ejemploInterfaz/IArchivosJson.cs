bb429f904e6bd9a36a0b74256ed07846bef6a9a7
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-07-24 12:55:29 +0200

    Hecho interfaz

---
+++ ejemploInterfaz/ejemploInterfaz/IArchivosJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ejemploInterfaz
+{
+    public interface IArchivosJson : IAlmacenamiento, Modelo.IAlmacenamiento
+    {
+        object Deserializar(string json);
+
+        void Serializar(object json);
+    }
+}
