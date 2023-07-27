ed2c6765a7c247572e1cbe1ebb6b1a2dc5d94732
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-07-27 09:24:26 +0200

    Creado proyecto Delegados

---
+++ I-Desarrollo para el backend .NET (core). Parte I/EjemploDelegadosEventosLinq/EjemploDelegados/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EjemploDelegados
+{
+
+    // Hay que crear una aplicación para 
+    // el archivo de medios de una biblioteca publica
+    // de modo que:
+    // 1-Segun el tipo de medio indique al usuario los pasos
+    //   a dar para reproducir ese medio y verificar 
+    //   si está en buen estado para archivarlo
+    //   o bien desecharlo si está en mal estado
+    // 2-Crear una aplicación que permita indicar
+    //   los pasos a dar independientemente del medio
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
