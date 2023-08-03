8144b8ddec211467f7fc9feec0ad03c43b1b287f
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-08-03 13:15:55 +0200

    Varios cambios

---
+++ III-Desarrollo para el backend .NET (core). Parte II/ConversorWebSolucion/ConversorWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ConversorWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ConversorWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.saludo = "Hola mundo desde c#";
+            ViewBag.aguacate = true;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ III-Desarrollo para el backend .NET (core). Parte II/ConversorWebSolucion/ConversorWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConversorWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
