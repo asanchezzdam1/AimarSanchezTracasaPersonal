c0c824d4d31f3b8e873ccaffd95c1db54b7101ea
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-08-10 13:41:15 +0200

    Varios cambios

--- III-Desarrollo para el backend .NET (core). Parte II/ProyectosWebSolucion/EjemploMvcConversor/Models/Moneda.cs
+++ III-Desarrollo para el backend .NET (core). Parte II/ProyectosWebSolucion/EjemploMvcConversor/Models/Moneda.cs
@@ -1,9 +1,15 @@
-﻿namespace EjemploMvcConversor.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EjemploMvcConversor.Models
 {
+    //[Table("Monedas")]
     public class Moneda
     {
+        [Key]
         public int Id { get; set; }
 
+        [MaxLength(5)]
         public string CodigoMoneda { get; set; }
 
         public string Descripcion { get; set; }
