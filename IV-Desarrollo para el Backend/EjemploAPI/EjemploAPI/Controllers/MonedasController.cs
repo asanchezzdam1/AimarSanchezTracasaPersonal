201124ddc4dbea6c2874f99819a8dbe14b556f72
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-08-16 14:05:30 +0200

    Varios cambios

---
+++ IV-Desarrollo para el Backend/EjemploAPI/EjemploAPI/Controllers/MonedasController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EjemploAPI.Controllers
+{
+
+    public class Moneda
+    {
+        public string nombre{ get; set; }
+    }
+
+    // 1- Ruta
+    //[Route("api/[controller]")]    
+    [Route("api/Monedas")]
+
+    //2-Controlador de tipo API
+    [ApiController]
+
+    public class MonedasController : ControllerBase //3-Hereda de controlador API
+    {
+        //Verbo Http
+        [HttpGet]
+
+        //Devuelve un ActionResult<Tipo>
+        public ActionResult<IEnumerable<Moneda>> obtenerMonedas()
+        {
+            var lista = new List<Moneda>();
+            lista.Add(new Moneda { nombre = "EUR" });
+
+            //Devolver resultado y codigo de estado
+            return Ok(lista);
+
+
+        }
+    }
+}
