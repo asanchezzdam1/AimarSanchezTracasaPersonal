096895623db27dca00c877978817c20d94b2eb6f
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-09-14 09:36:41 +0200

    Examen

--- Examen/API/Context/ContextoConversor.cs
+++ Examen/API/Context/ContextoConversor.cs
@@ -5,9 +5,7 @@ namespace Context
 {
     public class ContextoConversor : DbContext
     {
-        public ContextoConversor(DbContextOptions<ContextoConversor> opciones) : base(opciones)
-        {
-        }        
+        public ContextoConversor(DbContextOptions<ContextoConversor> opciones) : base(opciones){}     
         public DbSet<Usuario> usuarios { get; set; }
     }
 }
