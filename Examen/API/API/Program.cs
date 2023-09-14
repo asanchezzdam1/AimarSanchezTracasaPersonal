c7fb87141f38cc1ecb6085cb7f0baf41bb8e3a9e
asanchezzdam1 <asanchezzdam1@gmail.com>
2023-09-14 09:47:38 +0200

    Examen

--- Examen/API/API/Program.cs
+++ Examen/API/API/Program.cs
@@ -12,34 +12,31 @@ namespace API
             //++1
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllers(configure =>
-                    {
-                        configure.ReturnHttpNotAcceptable = true;
-                    })
-                    .AddNewtonsoftJson(setupAction =>
-                    {
-                        setupAction.SerializerSettings.ContractResolver =
-                            new CamelCasePropertyNamesContractResolver();
-                    });
+            builder.Services.AddControllers(configure => { configure.ReturnHttpNotAcceptable = true; })
+                            .AddNewtonsoftJson(setupAction =>
+                            {
+                                setupAction.SerializerSettings.ContractResolver =
+                                    new CamelCasePropertyNamesContractResolver();
+                            });
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             builder.Services.AddScoped<IRepositorioUsuarios, RepositorioUsuarios>();
             builder.Services.AddDbContext<ContextoConversor>(options =>
-                        {
-                            options.UseSqlServer(builder.Configuration["ConnectionStrings:ConexionDatos"]);
-                        });
+            {
+                options.UseSqlServer(builder.Configuration["ConnectionStrings:ConexionDatos"]);
+            });
             builder.Services.AddCors(options =>
-                         {
-                             options.AddPolicy("AllowAll",
-                                 builder =>
-                                 {
-                                     builder
+            {
+                options
+                            .AddPolicy("AllowAll", builder =>
+                            {
+                                builder
                                      .AllowAnyOrigin()
                                      .AllowAnyMethod()
                                      .AllowAnyHeader();
-                                 });
-                         });
+                            });
+            });
             //++2
             var app = builder.Build();
             if (app.Environment.IsDevelopment())
